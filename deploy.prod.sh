c55f375aa9a1fdd6030de6210d5358013af90a93
Chidindu Promise Ogbonna <promise.bones@gmail.com>
2021-02-17 08:08:52 +0100

    chore: Deployment scripts

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,1 +1,4 @@
-sam deploy --parameter-overrides "Stage=prod" -t template.yml
+API_ID=evsc9drhbc # Replace with your REST API ID
+STAGE_NAME=prod
+
+aws apigateway create-deployment --rest-api-id $API_ID --stage-name $STAGE_NAME
--- deploy.staging.sh
+++ deploy.staging.sh
@@ -1,4 +1,1 @@
-API_ID=evsc9drhbc
-STAGE_NAME=staging
-
-aws apigateway create-deployment --rest-api-id $API_ID --stage-name $STAGE_NAME
+sam deploy --parameter-overrides "Stage=staging" -t template.yml
